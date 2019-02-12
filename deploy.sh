aaf4d8fc8ff79ff89b9f00550a7faf2ed54d190b
njlr <njlr@users.noreply.github.com>
2019-02-12 13:37:42 +0000

    * Adds images to deployment

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@
 
 ORIGIN=`git remote get-url origin`
 MESSAGE=`git --no-pager log --format="%h: %s" -n1`
+mkdir -p dist
+cp -r img ./dist
 cd dist
 git init
+git remote add origin $ORIGIN
 git add .
 git commit -m "$MESSAGE"
 git stash
