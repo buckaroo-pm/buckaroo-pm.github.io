187807b78969ff5dacef2da38906e8dcc06a87de
Gaetano Checinski <gaetano.checinski@gmail.com>
2019-02-12 15:15:58 +0000

    add analytics

--- templates/default.js
+++ templates/default.js
@@ -1,3 +1,12 @@
+const analytics = ua  => `
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', ${ua}, 'buckaroo.pm');
+ga('send', 'pageview');
+`;
+
 export default ({ content, root = '', meta = { title: "blog" }, styles = [], scripts = [] }) => {
   styles = styles.slice().reverse();
 
@@ -17,8 +26,9 @@ export default ({ content, root = '', meta = { title: "blog" }, styles = [], scr
       <body>
         <div id="react-root">
         ${content}
-        </div>
-        ${scripts.map(url =>`<script type="text/javascript" src="${root}/${url}"></script>\n`)}
+      </div>
+      <script>${analytics("UA-98721445-1")}</script>
+      ${scripts.map(url =>`<script type="text/javascript" src="${root}/${url}"></script>\n`)}
       </body>
     </html>`
   );
