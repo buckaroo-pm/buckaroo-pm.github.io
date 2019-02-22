5c066103179f8afb97e365f4b6de90ca2b1275ec
njlr <njlr@users.noreply.github.com>
2019-02-22 11:00:27 +0000

    * Mixpanel page views

--- analytics.js
+++ analytics.js
@@ -1,9 +1,15 @@
 import { withRouter } from 'react-router';
 
 const onUpdate = () => {
-  if (typeof window !== undefined && window.ga) {
-    window.ga('set', 'page', window.location.pathname + window.location.search);
-    window.ga('send', 'pageview');
+  if (typeof window !== undefined) {
+    if (window.ga) {
+      window.ga('set', 'page', window.location.pathname + window.location.search);
+      window.ga('send', 'pageview');
+    }
+
+    if (window.mixpanel) {
+      window.mixpanel.track('Page View');
+    }
   }
 }
 
