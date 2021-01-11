aa82575df9c4d06bef79289a9de36b96603769c4
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2021-01-11 09:40:08 +0000

    remove old files

--- pages/posts/hello-world.md
+++
@@ -1,7 +0,0 @@
----
-title: "Hello, World. "
-created: "2019-02-10T00:00:00.000Z"
-author: Team Buckaroo
-summary: Why hello there.
----
-Why hello there...
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-require('@babel/register');
-
-module.exports = env => {
-  if (env && env.devServer) {
-    return require('./webpack.dev.config.js')
-  } else {
-    return require('./webpack.prod.config.js')
-  }
-}
